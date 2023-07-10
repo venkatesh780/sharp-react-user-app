39639a589508f364a91867470bdf866e15b58834
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-07-10 11:07:18 +0530

    initial commit - project setup done

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
