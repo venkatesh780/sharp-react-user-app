3fcf8e72141a6c18dbd4ec888cc5cc28a777d848
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-07-12 13:35:00 +0530

    used refs to get user entered data from input fields

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,47 +1,36 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Wrapper from "./Helpers/Wrapper";
 
 const UserForm = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userAge, setUserAge] = useState("");
+  const userNameRef = useRef();
+  const userAgeRef = useRef();
+  // const [userName, setUserName] = useState("");
+  // const [userAge, setUserAge] = useState("");
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
     const userData = {
-      userName,
-      userAge,
+      userName: userNameRef.current.value,
+      userAge: userAgeRef.current.value,
       id: `U${props.usersLength}`,
     };
     props.onSubmitUser(userData);
-    setUserName("");
-    setUserAge("");
-  };
-  const userNameHandler = (e) => {
-    setUserName(e.target.value);
-  };
-  const userAgeHandler = (e) => {
-    setUserAge(e.target.value);
+
+    userNameRef.current.value = "";
+    userAgeRef.current.value = "";
   };
   return (
     <Wrapper>
+      {console.log("userform-rendering")}
       <div className="bg-green-100 p-2 m-2 max-w-lg">
         <form>
           <div className="m-3">
             <label className="block">User Name</label>
-            <input
-              type="text"
-              className="h-8"
-              value={userName}
-              onChange={userNameHandler}
-            />
+            <input type="text" className="h-8" ref={userNameRef} />
           </div>
           <div className="m-3">
             <label className="block">Age (Years)</label>
-            <input
-              type="number"
-              className="h-8"
-              value={userAge}
-              onChange={userAgeHandler}
-            />
+            <input type="number" className="h-8" ref={userAgeRef} />
           </div>
           <button
             type="submit"
